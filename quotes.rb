ec9fd8b651ba2b4955f9170c29ef237961cca760
madebycm <christian.meinhold@gmail.com>
2013-11-08 14:06:21 +0100

    v1

---
+++ quotes.rb
@@ -0,0 +1,35 @@
+require 'open-uri'
+require 'mongo'
+include Mongo
+
+mongo = MongoClient.new()
+@db = mongo.db('quotes')
+
+"Now inserting 1000 quotes to the database"
+for i in 0..1000
+	output = ''
+	open("http://iheartquotes.com/api/v1/random") { |f|
+		f.each_line {|line| output << line}
+	}
+	# remove advertisement on last line
+	output = output.split("\n")
+	output.pop
+	output = output.join("")
+	p "====== CONGRATS. HAS A QUOTE " + i.to_s + " ===="
+	p output
+	p "==================================="
+	check = @db.collection('q').find_one({
+		"quote" => output
+	}).to_a.length
+	if check > 0
+		p "   == [Quote already in DB, skip...]"
+		next
+	end
+	# insert
+	insert = @db.collection('q').insert({
+		"quote" => output})
+	p "======   INSERT RECEIPT     ======"
+	p insert
+	p "=================================="
+end
+p "Operation complete"
